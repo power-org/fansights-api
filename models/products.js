d8f577e1477071317e488865357446feea9cc39c
Ken Crucillo <ken@dxmake.local>
2019-04-21 06:47:57 +0800

    FEAT: upload to s3 then insert record to database

--- models/products.js
+++ models/products.js
@@ -1,10 +1,11 @@
-const { Database } = require('../lib');
-const async = require('async');
+const { Database } = require("../lib");
+const async = require("async");
 
 let products = {
-    getProductsByTag: (payload)=>{
-        return new Promise((resolve, reject)=>{
-            Database.execute(`
+  getProductsByTag: payload => {
+    return new Promise((resolve, reject) => {
+      Database.execute(
+        `
             SELECT fd.id, t.name, fd.long_desc FROM food_des fd
             LEFT JOIN fd_group fdg ON fdg.code = fd.group_code
             LEFT JOIN footnote fn ON fn.ndb_no = fd.ndb_no
@@ -13,16 +14,64 @@ let products = {
             WHERE t.name IN (?)
             GROUP BY fd.id, t.name, fd.long_desc
             ORDER BY t.name
-            `, [payload]).then(data=>{
-                var grouped = data.groupBy(function (item) {
-                    return [item.name]; 
-                });
-                resolve(grouped);
-            }).catch(error=>{
-                reject(error);
-            })
+            `,
+        [payload]
+      )
+        .then(data => {
+          var grouped = data.groupBy(function(item) {
+            return [item.name];
+          });
+          resolve(grouped);
+        })
+        .catch(error => {
+          reject(error);
         });
-    }
+    });
+  },
+  save: payload => {
+    return new Promise((resolve, reject) => {
+      async.auto({
+          master: function(callback) {
+            Database.execute(`INSERT INTO meal_master SET ?`, payload[0]).then(data=>{
+              if(data.affectedRows > 0){
+                callback(null, data.insertId);
+              }else{
+                callback({
+                  error: "Failed to insert data."
+                })
+              }
+            }).catch(error=>{
+              callback(error)
+            });
+          },
+          details: ["master", function(result, callback) {
+            let queries = [];
+            let params = payload[1].map(e=>{
+              queries.push(`INSERT INTO meal_details SET ?;`);
+              e.mm_id = result.master;  //master id
+              if(e.tag instanceof Array) e.tag = e.tag.join(',');
+              return e;
+            });
+            Database.execute(queries.join(''), params).then(data=>{
+              callback(null, data);
+            }).catch(error=>{
+              callback(error)
+            });
+          }]
+        },
+        function(err, result) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve({
+              master: result.master,
+              details: result.details
+            });
+          }
+        }
+      );
+    });
+  }
 };
 
 module.exports = products;
