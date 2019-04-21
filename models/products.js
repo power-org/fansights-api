2b8cb62618e8166287c0d9f4a1643c21fc3a8a68
Ken Crucillo <ken@dxmake.local>
2019-04-21 08:56:07 +0800

    FIX: return product with nutrional data only

--- models/products.js
+++ models/products.js
@@ -6,15 +6,13 @@ let products = {
     return new Promise((resolve, reject) => {
       Database.execute(
         `
-            SELECT fd.id, t.name, fd.long_desc FROM food_des fd
-            LEFT JOIN fd_group fdg ON fdg.code = fd.group_code
-            LEFT JOIN footnote fn ON fn.ndb_no = fd.ndb_no
-            LEFT JOIN weight w ON w.ndb_no = fd.ndb_no
-            LEFT JOIN tags t ON fd.short_desc regexp t.name
-            WHERE t.name IN (?)
-            GROUP BY fd.id, t.name, fd.long_desc
-            ORDER BY t.name
-            `,
+        SELECT fd.id, fd.ndb_no, t.name, fd.long_desc FROM food_des fd
+        LEFT JOIN tags t ON fd.short_desc regexp t.name
+        LEFT JOIN nut_data nd ON nd.ndb_no = fd.ndb_no
+        WHERE t.name IN (?) AND nd.ndb_no is not null
+        GROUP BY fd.id, fd.ndb_no, t.name, fd.long_desc
+        ORDER BY t.name
+        `,
         [payload]
       )
         .then(data => {
