663332d371f914a13cbeb5122f9c2d918aaa10ab
Ken Crucillo <ken@dxmake.local>
2019-04-22 07:59:12 +0800

    FIX: removed logger in session

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@
 module.exports.isAuth = (req, res, next) => {
   const ACTION = `[isAuthenticated]`;
   // Get user token
-  console.log("info", `[AUTH]${ACTION} - user session`, req.session);
+  // console.log("info", `[AUTH]${ACTION} - user session`, req.session);
   if(req.session.user){
     next();
   }else{
@@ -14,7 +14,7 @@ module.exports.isAuth = (req, res, next) => {
 module.exports.isAuthAPI = (req, res, next) => {
   const ACTION = `[isAuthenticated]`;
   // Get user token
-  console.log("info", `[AUTH]${ACTION} - user session`, req.session);
+  // console.log("info", `[AUTH]${ACTION} - user session`, req.session);
   if(req.session.user){
     next();
   }else{
@@ -27,7 +27,7 @@ module.exports.isAuthAPI = (req, res, next) => {
 module.exports.isAfterAuthAPI = (req, res, next) => {
   const ACTION = `[isAuthenticated]`;
   // Get user token
-  console.log("info", `[AUTH]${ACTION} - user session`, req.session);
+  // console.log("info", `[AUTH]${ACTION} - user session`, req.session);
   if(req.session.user){
     res.status(400).json({
       message: 'Already logged in.'
@@ -40,7 +40,7 @@ module.exports.isAfterAuthAPI = (req, res, next) => {
 module.exports.hasSession = (req, res, next) => {
   const ACTION = `[hasSession]`;
   // Get user token
-  console.log("info", `[AUTH]${ACTION} - user session`, req.session);
+  // console.log("info", `[AUTH]${ACTION} - user session`, req.session);
   if(req.session.user){
     res.redirect('/');
   }else{
