bcdb9765590b0a341d6fc987c7673d2360d3e690
Ken Crucillo <ken@dxmake.local>
2019-04-21 20:51:09 +0800

    FIX: sum the result of get meal details

--- models/home.js
+++ models/home.js
@@ -102,35 +102,74 @@ let home = {
         });
       });
     },
-    getMealDetails: (meal_id)=>{
+    getMealDetails: (user_id, meal_id)=>{
       return new Promise((resolve, reject) => {
-        Database.execute(`
-        SELECT
-          md.id,
-          md.tag,
-          md.mm_id,
-          fd.id as products_id, fd.ndb_no, fd.long_desc,
-          nd.nutr_val,
-          nrd.units,
-          nrd.tagname,
-          nrd.nutr_desc,
-          nrd.num_dec,
-          nrd.sr_order
-        FROM meal_details md
-        LEFT JOIN food_des fd ON fd.id = md.products_id
-        LEFT JOIN fd_group fdg ON fdg.code = fd.group_code
-        LEFT JOIN nut_data nd ON nd.ndb_no = fd.ndb_no
-        LEFT JOIN nutr_def nrd ON nrd.nutr_no = nd.nutr_no
-        WHERE md.mm_id IN (?) AND nd.nutr_no IN(208,268,203,204,205,291)
-        ORDER BY md.id, nrd.sr_order
-        `, meal_id).then(data=>{
-          let grouped = data.groupBy(function(item) {
-            return [item.tag];
-          });
-          resolve(grouped);
-        }).catch(error=>{
-          reject(error);
-        })
+        async.auto({
+          getMeals: function(callback){
+            Database.execute(`SELECT * FROM meal_master WHERE member_id = ? AND id = ?`, [user_id, meal_id]).then(data=>{
+              callback(null, data);
+            }).catch(error=>{
+              callback(error)
+            });
+          },
+          getMealsDetails: ['getMeals', function(result, callback){
+            if(result.getMeals.length < 1) return callback(null, []);
+            Database.execute(`
+            SELECT
+              md.id,
+              md.mm_id,
+              md.tag,
+              fd.id products_id,
+              fd.ndb_no,
+              fd.long_desc
+            FROM meal_details md
+            LEFT JOIN food_des fd ON fd.id = md.products_id
+            WHERE md.mm_id IN(?)`, [result.getMeals.map(e=>e.id)]).then(data=>{
+              callback(null, data);
+            }).catch(error=>{
+              callback(error)
+            });
+          }],
+          getCalorieCount: ['getMealsDetails', function(result, callback){
+            if(result.getMeals.length === 0) return callback(null, []);
+            Database.execute(`
+              SELECT
+                SUM(nd.nutr_val) nutr_val,
+                nrd.units,
+                nrd.tagname,
+                nrd.nutr_desc,
+                nrd.sr_order
+              FROM meal_master m
+              LEFT JOIN meal_details md ON md.mm_id = m.id
+              LEFT JOIN food_des fd ON fd.id = md.products_id
+              LEFT JOIN fd_group fdg ON fdg.code = fd.group_code
+              LEFT JOIN nut_data nd ON nd.ndb_no = fd.ndb_no
+              LEFT JOIN nutr_def nrd ON nrd.nutr_no = nd.nutr_no
+              WHERE m.member_id = ? AND m.id = ? AND nd.nutr_no IN(208,268,203,204,205,291)
+              GROUP BY nrd.units, nrd.tagname, nrd.nutr_desc, nrd.sr_order
+              ORDER BY nrd.sr_order
+            `, [user_id, meal_id]).then(data=>{
+              callback(null, data);
+            }).catch(error=>{
+              callback(error)
+            });
+          }]
+        }, function(err, result){
+          if(err){
+            reject(err);
+          }else{
+            if(result.getMeals.length > 0){
+              let meal = result.getMeals.map(e=>{
+                e.details = result.getMealsDetails.filter(m=>m.mm_id === e.id);
+                return e;
+              });
+              meal[0].nutrients = result.getCalorieCount;
+              resolve(meal[0]);
+            }else{
+              reject([]);
+            }
+          }
+        });
       });
     }
 };
